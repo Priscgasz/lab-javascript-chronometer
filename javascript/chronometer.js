8ff9fffaee7857973546f24b7499e15eb66b7ee2
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-10-04 12:02:15 +0200

    chronometer lab

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,37 +1,54 @@
 class Chronometer {
   constructor() {
+    this.currentTime = 0; 
+    this.intervalId = null;
     // ... your code goes here
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime +=1;
+    },1000); 
   }
+    // ... your code goes here
 
   getMinutes() {
+    let numberOfMinutes = Math.floor(this.currentTime / 60); 
+    return numberOfMinutes;
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime%60;  
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) { 
+    return `0${value}`;
+    } else {
+      return `${value}`;
+    }
   }
+    // ... your code goes here
 
   stop() {
+    clearInterval(this.intervalId);
     // ... your code goes here
   }
 
   reset() {
+    this.currentTime = 0
     // ... your code goes here
   }
 
   split() {
+    let formatMinutes = this.computeTwoDigitNumber(this.getMinutes()); 
+    let formatSeconds = this.computeTwoDigitNumber(this.getSeconds()); 
+    return`${formatMinutes}:${formatSeconds}`; 
     // ... your code goes here
   }
 }
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
